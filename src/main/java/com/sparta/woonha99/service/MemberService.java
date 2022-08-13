87c76a46b85932f5a3127c1b45a670b61a094547
shinyounseob1022 <83537005+shinyounseob1022@users.noreply.github.com>
2022-08-13 17:58:45 +0900

    로그인 기능 구현 완료

--- src/main/java/com/sparta/woonha99/service/MemberService.java
+++ src/main/java/com/sparta/woonha99/service/MemberService.java
@@ -1,15 +1,22 @@
 package com.sparta.woonha99.service;
 
 import com.sparta.woonha99.domain.Member;
+import com.sparta.woonha99.dto.request.LoginRequestDto;
 import com.sparta.woonha99.dto.request.MemberRequestDto;
+import com.sparta.woonha99.dto.request.TokenDto;
 import com.sparta.woonha99.dto.response.MemberResponseDto;
 import com.sparta.woonha99.dto.response.ResponseDto;
+import com.sparta.woonha99.jwt.TokenProvider;
 import com.sparta.woonha99.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
 @Service
@@ -18,6 +25,8 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final TokenProvider tokenProvider;
 
     @Transactional
     public ResponseDto<?> createMember(MemberRequestDto requestDto) {
@@ -46,10 +55,40 @@ public class MemberService {
         );
     }
 
+    @Transactional
+    public ResponseDto<?> login(LoginRequestDto requestDto, HttpServletResponse response) {
+        Member member = isPresentMember(requestDto.getNickname());
+        if (null == member) {
+            return ResponseDto.fail("MEMBER_NOT_FOUND",
+                    "존재하지 않는 아이디입니다.");
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(requestDto.getNickname(), requestDto.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+        tokenToHeaders(tokenDto, response);
+
+        return ResponseDto.success(
+                MemberResponseDto.builder()
+                        .createdAt(member.getCreatedAt())
+                        .modifiedAt(member.getModifiedAt())
+                        .msg("로그인 성공")
+                        .build()
+        );
+    }
+
     @Transactional(readOnly = true)
     public Member isPresentMember(String nickname) {
         Optional<Member> optionalMember = memberRepository.findByNickname(nickname);
         return optionalMember.orElse(null);
+    }
+
+    public void tokenToHeaders(TokenDto tokenDto, HttpServletResponse response) {
+        response.addHeader("Access-Token", "Bearer " + tokenDto.getAccessToken());
+        response.addHeader("Refresh-Token", "Bearer " + tokenDto.getRefreshToken());
+        response.addHeader("Access-Token-Expire-Time", tokenDto.getAccessTokenExpiresIn().toString());
     }
 
 }
