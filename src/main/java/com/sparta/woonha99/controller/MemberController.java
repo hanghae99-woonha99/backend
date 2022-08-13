47bd8094453f9ac0a3ccb58f3cb91eb8ed2b266f
shinyounseob1022 <83537005+shinyounseob1022@users.noreply.github.com>
2022-08-13 16:08:34 +0900

    회원가입 기능 구현 완료

--- src/main/java/com/sparta/woonha99/controller/MemberController.java
+++ src/main/java/com/sparta/woonha99/controller/MemberController.java
@@ -1,13 +1,23 @@
 package com.sparta.woonha99.controller;
 
+import com.sparta.woonha99.dto.request.MemberRequestDto;
+import com.sparta.woonha99.dto.response.ResponseDto;
 import com.sparta.woonha99.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseDto<?> signup(@RequestBody @Valid MemberRequestDto requestDto) {
+        return memberService.createMember(requestDto);
+    }
 
 }
